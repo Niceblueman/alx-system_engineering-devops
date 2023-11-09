7dee27fc7274944a01c60da0546946c997e42fbd
Niceblueman <wintheiser761@gmail.com>
2023-11-09 15:08:54 +0100

    done 100

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -2,89 +2,72 @@
 """
 Function that queries the Reddit API and prints
 """
-import re
+import concurrent.futures
 import requests
-import sys
+import threading
+
+word_count = {}
+lock = threading.Lock()
 
 
-def add_title(dictionary, hot_posts):
-    """_summary_
-
-    Args:
-        dictionary (_type_): _description_
-        hot_posts (_type_): _description_
-    """
-    if len(hot_posts) == 0:
-        return
-
-    title = hot_posts[0]['data']['title'].split()
-    for word in title:
-        for key in dictionary.keys():
-            c = re.compile("^{}$".format(key), re.I)
-            if c.findall(word):
-                dictionary[key] += 1
-    hot_posts.pop(0)
-    add_title(dictionary, hot_posts)
-
-
-def recurse(subreddit, dictionary, after=None):
-    """_summary_
-
-    Args:
-        subreddit (_type_): _description_
-        dictionary (_type_): _description_
-        after (_type_, optional): _description_. Defaults to None.
-
-    Returns:
-        _type_: _description_
-    """
-    u_agent = 'Mozilla/5.0'
-    headers = {
-        'User-Agent': u_agent
-    }
-
-    params = {
-        'after': after
-    }
-
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    res = requests.get(url,
-                       headers=headers,
-                       params=params,
-                       allow_redirects=False)
-
-    if res.status_code != 200:
-        return None
-
-    dic = res.json()
-    hot_posts = dic['data']['children']
-    add_title(dictionary, hot_posts)
-    after = dic['data']['after']
-    if not after:
-        return
-    recurse(subreddit, dictionary, after=after)
-
-
-def count_words(subreddit, word_list):
+def process_posts(subreddit, word_list, posts):
     """_summary_
 
     Args:
         subreddit (_type_): _description_
         word_list (_type_): _description_
+        posts (_type_): _description_
     """
-    dictionary = {}
+    global word_count
+    with lock:
+        for post in posts:
+            title = post["data"]["title"].lower()
+            for word in word_list:
+                word = word.lower()
+                if (
+                    title.count(word) > 0
+                    and f" {word} " in title
+                    and not (title.startswith(f"{word} ") or title.endswith(f" {word}"))
+                ):
+                    if word in word_count:
+                        word_count[word] += title.count(word)
+                    else:
+                        word_count[word] = title.count(word)
 
-    for word in word_list:
-        dictionary[word] = 0
 
-    recurse(subreddit, dictionary)
+def count_words(subreddit, word_list, after=None):
+    """_summary_
 
-    l = sorted(dictionary.items(), key=lambda kv: kv[1])
-    l.reverse()
+    Args:
+        subreddit (_type_): _description_
+        word_list (_type_): _description_
+        after (_type_, optional): _description_. Defaults to None.
+    """
+    global word_count
 
-    if len(l) != 0:
-        for item in l:
-            if item[1] is not 0:
-                print("{}: {}".format(item[0], item[1]))
+    if after is None:
+        response = requests.get(
+            f"https://www.reddit.com/r/{subreddit}/hot/.json",
+            headers={"User-agent": "count_words"},
+        )
     else:
-        print("")
+        response = requests.get(
+            f"https://www.reddit.com/r/{subreddit}/hot/.json?after={after}",
+            headers={"User-agent": "count_words"},
+        )
+    if response.status_code != 200:
+        return
+    data = response.json()
+    posts = data["data"]["children"]
+    with concurrent.futures.ThreadPoolExecutor(max_workers=len(word_list)) as executor:
+        executor.submit(process_posts, subreddit, word_list, posts)
+
+    after = data["data"]["after"]
+    if after is not None:
+        count_words(subreddit, word_list, after)
+    else:
+        sorted_word_count = sorted(
+            word_count.items(), key=lambda x: (-x[1], x[0])
+        )
+        for word, count in sorted_word_count:
+            print(f"{word}: {count}")
